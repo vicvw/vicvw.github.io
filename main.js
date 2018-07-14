9c0a16d02e067608ffc2b8ed0604e7acc29f2866
Victor Wiatr <victor.wiatr+github@gmail.com>
2018-07-14 03:20:24 +0200

    sagasu

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+$(document).ready(() => {
+  focus($('#ireru'));
+
+  $.each(s, (k, v) => {
+    const a = $('<a></a>').text(k).attr({
+        name:   k,
+        target: '_blank' });
+
+    $('#sagasu').append(a);
+  });
+})
+
+
 Mousetrap.bind('space', () => {
   focus('#ireru');
 }, 'keyup');
@@ -8,7 +21,7 @@ $('#ireru').on('input', () => {
   const isbnOut = $('#isbn-out');
   const r       = {
       isbn:   /^\d{13}$/,
-      jidai:  /^([mtsh]?)(\d{1,3})$/
+      jidai:  /^([mtsh]?)([1-9]\d{0,2})$/
     }
 
   if (r.isbn.test(input)) {
@@ -17,7 +30,7 @@ $('#ireru').on('input', () => {
     toClipboard(isbn);
     focus('#ireru');
     isbnOut.text(isbn);
-    isbnOut.attr('href', google('ean '.concat(input)));
+    isbnOut.attr('href', s.google('ean '.concat(input)));
     return;
   }
   else if (r.jidai.test(input)) {
@@ -38,7 +51,11 @@ $('#ireru').on('input', () => {
 
   const hz = hanzi();
   isbnOut.text(hz);
-  isbnOut.attr('href', google(hz));
+  isbnOut.attr('href', s.google(hz));
+
+  $('#sagasu a').attr('href', function() {
+    return s[this.name](input);
+  });
 });
 
 
@@ -60,14 +77,12 @@ $('#ireru').keyup(e => {
 
 
 const toIsbn = x =>
-  /^\d{13}$/.test(x)
-    ? [ x.substr( 0, 3),
-        x.substr( 3, 1),
-        x.substr( 4, 3),
-        x.substr( 7, 5),
-        x.substr(12, 1)
-      ].join('-')
-    : false;
+  [ x.substr( 0, 3),
+    x.substr( 3, 1),
+    x.substr( 4, 3),
+    x.substr( 7, 5),
+    x.substr(12, 1)
+  ].join('-')
 
 
 const jidai = (g, nen) => {
@@ -81,40 +96,40 @@ const jidai = (g, nen) => {
   const [gou, gouR, ad] = jd[g];
   const AD = (ad - 1) + parseInt(nen);
 
-  return [ gou.concat((g === 'mg') ? toKJ(nen)[0] : nen,
-                      '年'),
-           gouR.concat(' ',
-                       (g === 'mg') ? toKJ(nen)[1] : nen,
-                       ' ',
-                       (g === 'mg') ? 'nian' : 'nen'),
+  return [ gou.concat(nen, '年'),
+           gouR.concat(' ', nen, ' ', (g === 'mg') ? 'nian' : 'nen'),
            AD
          ];
 };
 
 
-const toKJ = s => {
-  const digit = [
-    ['零', 'ling'],
-    ['一', 'yi'],
-    ['二', 'er'],
-    ['三', 'san'],
-    ['四', 'si'],
-    ['五', 'wu'],
-    ['六', 'liu'],
-    ['七', 'qi'],
-    ['八', 'ba'],
-    ['九', 'jiu']];
+// const toKJ = s => {
+//   const digit = [
+//     ['零', 'ling'],
+//     ['一', 'yi'],
+//     ['二', 'er'],
+//     ['三', 'san'],
+//     ['四', 'si'],
+//     ['五', 'wu'],
+//     ['六', 'liu'],
+//     ['七', 'qi'],
+//     ['八', 'ba'],
+//     ['九', 'jiu']];
 
-  const arr = s.split('');
-  const k = arr.map(x => digit[parseInt(x)][0]).join('');
-  const r = arr.map(x => digit[parseInt(x)][1]).join(' ');
+//   const arr = s.split('');
+//   const k = arr.map(x => digit[parseInt(x)][0]).join('');
+//   const r = arr.map(x => digit[parseInt(x)][1]).join(' ');
 
-  return [k, r];
+//   return [k, r];
+// };
+
+
+const s = {
+  google:   x => 'https://www.google.de/search?q='.concat(x),
+  worldcat: x => 'http://www.worldcat.org/search?qt=worldcat_org_all&q='.concat(x),
+  jstor:    x => 'https://www.jstor.org/action/doBasicSearch?Query='.concat(x, '&acc=off&wc=on&fc=off&group=none'),
+  amazon:   x => 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords='.concat(x)
 };
-
-
-const google = x =>
-  'https://www.google.de/search?q='.concat(x);
 
 
 const hanzi = () =>
